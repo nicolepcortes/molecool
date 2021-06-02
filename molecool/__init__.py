0beae5b6664360603d44ee8acb0506e2f113bf2c
Nicole Cortes <ncortes@nd.edu>
2021-06-02 12:32:07 -0400

    updated import

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,7 +4,7 @@ A python package for analyzing and visualizing .xyz files.
 """
 
 # Add imports here
-from .functions import canvas
+from .functions import *
 
 # Handle versioneer
 from ._version import get_versions
